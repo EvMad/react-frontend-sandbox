8563c9940620f3d453383a98cb6f0599ca24635e
GitPit <rifflifter@gmail.com>
2022-04-27 13:22:01 -0400

    added single post image

--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -4,8 +4,8 @@ export default function SinglePost() {
     return (
         <div className="singlePost">
             <div className="singlePostWrapper">
-                <img src="" alt="" className="singlePostImg">
-                    
+                <img src="istockphoto-1179480831-170667a.jpg" alt="fishing net" className="singlePostImg">
+
                 </img>
             </div>
         </div>
