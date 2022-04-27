59122271537706505758af4491f1fffacaeb0456
GitPit <rifflifter@gmail.com>
2022-04-27 14:24:48 -0400

    .writeIcon {}

--- src/pages/write/Write.jsx
+++ src/pages/write/Write.jsx
@@ -7,7 +7,7 @@ export default function Write() {
             <form className="writeForm">
                 <div className="writeFormGroup">
                     <label htmlFor="fileInput">
-                    <i class="fa-solid fa-plus"></i>
+                    <i className="writeIcon fa-solid fa-plus"></i>
                     </label>
                     <input type="file" id="fileInput" style={{display:"none"}} />
                     <input type="text" placeholder="Title" className="writeInput" autoFocus={true} />
@@ -15,7 +15,7 @@ export default function Write() {
                 <div className="writeFormGroup">
                     <textarea placeholder="Tell your story..." type="text" className="writeInput writeText"></textarea>
                 </div>
-                <button className="writeSubmit"></button>
+                <button className="writeSubmit">Publish</button>
             </form>
         </div>
     )
