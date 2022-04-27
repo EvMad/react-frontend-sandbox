0602d8f0e9e24743bdd8455a9efa1340d2420edd
GitPit <rifflifter@gmail.com>
2022-04-27 14:08:29 -0400

    <form className="writeForm">

--- src/pages/write/Write.jsx
+++ src/pages/write/Write.jsx
@@ -4,7 +4,7 @@ import "./write.css";
 export default function Write() {
     return (
         <div className="write">
-            
+            <form className="writeForm"></form>
         </div>
     )
 }
