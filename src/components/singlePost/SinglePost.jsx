c560a34e6df6115d3e6d9f966a4b8f01e6e5529f
GitPit <rifflifter@gmail.com>
2022-04-27 13:27:18 -0400

    postTitle h1 with icons

--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -7,7 +7,13 @@ export default function SinglePost() {
                 <img src="istockphoto-1179480831-170667a.jpg" alt="fishing net" className="singlePostImg">
 
                 </img>
-                <h1 className="singlePostTitle"></h1>
+                <h1 className="singlePostTitle">
+                    Ut enim ad minima veniam 
+                    <div className="singlePostEdit">
+                    <i class="fa-solid fa-pen-to-square"></i>
+                    <i class="fa-solid fa-circle-trash"></i>
+                    </div>
+                </h1>
             </div>
         </div>
     )
