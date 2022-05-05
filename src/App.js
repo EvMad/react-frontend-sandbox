f8f84f379c87f57159d1e01c82cdc3f2dff5226c
GitPit <rifflifter@gmail.com>
2022-05-05 14:38:44 -0400

    import Setting from "./pages/settings/Setting";

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import TopBar from "./components/topbar/TopBar";
 import Home from "./pages/home/Home";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
+import Setting from "./pages/settings/Setting";
 
 
 function App() {
   return (
     <>
     <TopBar/>
-    <Write/>
+    <Setting/>
     </>
     
   );
