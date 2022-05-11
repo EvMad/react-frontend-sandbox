c4a3879474035c2b2f1c056120f637d6badc6586
GitPit <rifflifter@gmail.com>
2022-05-11 17:24:19 -0400

    created login form inputs

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -3,7 +3,13 @@ import "./login.css";
 export default function Login() {
     return (
         <div className="login">
-            
+            <form className="loginForm">
+                <label>Email</label>
+                <input type="text" placeholder="Enter your email..."></input>
+                <label>Password</label>
+                <input type="password" placeholder="Enter your pass..."></input>
+            </form>
+
         </div>
     )
 }
