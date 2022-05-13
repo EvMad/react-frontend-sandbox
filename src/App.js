a608ebf055ca058ae8de6875458c443b1d3bac4e
GitPit <rifflifter@gmail.com>
2022-05-13 14:51:18 -0400

    { user ? <Settings /> : <Register /> }

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
 
       <Route path="/write" element={ user ? <Write /> : <Register /> }></Route>
 
-      <Route path="/settings" element={<Settings />}></Route>
+      <Route path="/settings" element={ user ? <Settings /> : <Register /> }></Route>
       
      
     </Routes>
