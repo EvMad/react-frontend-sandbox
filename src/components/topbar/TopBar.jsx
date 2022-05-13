eaf6780d983fa307acc81a4695c540efdd7de4f7
GitPit <rifflifter@gmail.com>
2022-05-13 14:55:14 -0400

    import { Link } from "react-router-dom";

--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import './topbar.css'
 
 export default function TopBar() {
@@ -11,7 +12,7 @@ export default function TopBar() {
             </div>
             <div className="topCenter">
                 <ul className="topList">
-                    <li className="topListItem">HOME</li>
+                    <li className="topListItem"><Link to="/" style={{textDecoration:"none"}}>HOME</Link></li>
                     <li className="topListItem">ABOUT</li>
                     <li className="topListItem">CONTACT</li>
                     <li className="topListItem">WRITE</li>
